d765c56b17b0e3e8647a62568b089a835c5e0099
Petr Chatrný <p.chatrny@protonmail.com>
2023-04-13 16:19:38 +0200

    init

---
+++ src/main/java/cz/mendelu/pef/xchatrny/tolkiendictionaryapi/TolkienDictionaryAPI.java
@@ -0,0 +1,11 @@
+package cz.mendelu.pef.xchatrny.tolkiendictionaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolkienDictionaryAPI {
+    public static void main(String[] args) {
+        SpringApplication.run(TolkienDictionaryAPI.class, args);
+    }
+}
---
+++ src/test/java/cz/mendelu/pef/xchatrny/tolkiendictionaryapi/TolkienDictionaryAPITests.java
@@ -0,0 +1,13 @@
+package cz.mendelu.pef.xchatrny.tolkiendictionaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolkienDictionaryAPITests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
