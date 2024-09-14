3266cc1f8c6ea71467826f0ecbf9aa684af72961
Petr Chatrný <posta@petrchatrny.cz>
2024-09-14 08:57:34 +0200

    Update OpenApiConfig

--- src/main/java/cz/mendelu/pef/xchatrny/tolkienapi/config/OpenApiConfig.java
+++ src/main/java/cz/mendelu/pef/xchatrny/tolkienapi/config/OpenApiConfig.java
@@ -1,30 +1,33 @@
-package cz.mendelu.pef.xchatrny.tolkienapi.common.config;
-
+package cz.mendelu.pef.xchatrny.tolkienapi.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Swagger documentation configuration
+ * Configuration of generated OpenAPI specification
  */
 @Configuration
 public class OpenApiConfig {
+
+    private BuildProperties buildProperties;
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Tolkien API")
-                        .version("1.0")
+                        .version(buildProperties.getVersion())
                         .description("This web is a backend REST API for my Android application")
                         .contact(
                                 new Contact()
                                         .name("developer - Petr Chatrný")
-                                        .email("p.chatrny@protonmail.com")
+                                        .email("posta@petrchatrny.cz")
                         )
                         .license(
                                 new License()
@@ -32,5 +35,10 @@ public class OpenApiConfig {
 //                                        .url("https://akela.mendelu.cz/~xchatrny/soubory/licences/tolkien_dictionary_api.txt")
                         )
                 );
+    }
+
+    @Autowired
+    public void setBuildProperties(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
     }
 }
